51df7a0b46560f049bbb3cd91d64414f06609c2c
Gaurav Pandey <gaurav02712@gmail.com>
2022-11-15 22:34:22 +0530

    start: Initial commit with basic project

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Gaurav Pandey on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
